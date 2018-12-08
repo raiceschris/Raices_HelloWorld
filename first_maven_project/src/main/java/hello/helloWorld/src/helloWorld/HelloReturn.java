61ff32e975c1fd3f466a9995cea2283f6660e9a8
raiceschris <raiceschris@yahoo.com>
2018-12-08 13:29:25 -0500

    added project folder

---
+++ first_maven_project/src/main/java/hello/helloWorld/src/helloWorld/HelloReturn.java
@@ -0,0 +1,7 @@
+package helloWorld;
+
+public class HelloReturn {
+	public String hey() {
+		return "Say hi from different class";
+	}
+}
---
+++ first_maven_project/src/main/java/hello/helloWorld/src/helloWorld/helloWorld.java
@@ -0,0 +1,8 @@
+package helloWorld;
+
+public class helloWorld {
+	public static void main(String[] args) {
+        HelloReturn hello = new HelloReturn();
+		System.out.println(hello.hey());
+    }
+}
